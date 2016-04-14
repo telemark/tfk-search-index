84ace49198cd74d21b71e20983a981a5144afe5b
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-04-14 09:51:45 +0200

    Adds index to config. Updates engines. Updates version

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@
 var config = {
   JWT_KEY: process.env.JWT_KEY || 'Louie Louie, oh no, I got to go',
   SEARCH_SERVICE_URL: process.env.SEARCH_SERVICE_URL || 'https://search.service.com/api',
+  SEARCH_SERVICE_INDEX: process.env.SEARCH_SERVICE_INDEX || 'www',
   SEARCH_SERVICE_INDEX_TYPE: process.env.SEARCH_SERVICE_INDEX_TYPE || 'article',
   SITEMAP_URL: process.env.SITEMAP_URL || 'http://www.yoursite.com/sitemap.xml'
 }
